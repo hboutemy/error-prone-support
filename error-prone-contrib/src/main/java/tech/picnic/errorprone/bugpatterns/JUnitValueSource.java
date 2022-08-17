c17f70b4a55e76bc91ec5afce0e1cf720578de0d
Gijs de Jong <berichtaangijs@gmail.com>
2022-08-17 10:12:11 +0200

    Remove `.orElse(null)` usage

--- error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/JUnitValueSource.java
+++ error-prone-contrib/src/main/java/tech/picnic/errorprone/bugpatterns/JUnitValueSource.java
@@ -4,9 +4,9 @@ import static com.google.auto.common.MoreStreams.toImmutableList;
 import static com.google.errorprone.BugPattern.LinkType.NONE;
 import static com.google.errorprone.BugPattern.SeverityLevel.SUGGESTION;
 import static com.google.errorprone.BugPattern.StandardTags.SIMPLIFICATION;
+import static com.google.errorprone.matchers.Matchers.isType;
 import static com.sun.source.tree.Tree.Kind.MEMBER_SELECT;
 import static com.sun.source.tree.Tree.Kind.METHOD;
-import static com.sun.source.tree.Tree.Kind.METHOD_INVOCATION;
 import static com.sun.source.tree.Tree.Kind.RETURN;
 import static java.util.stream.Collectors.joining;
 
@@ -21,7 +21,6 @@ import com.google.errorprone.fixes.SuggestedFix;
 import com.google.errorprone.matchers.AnnotationMatcherUtils;
 import com.google.errorprone.matchers.Description;
 import com.google.errorprone.matchers.Matcher;
-import com.google.errorprone.matchers.Matchers;
 import com.google.errorprone.util.ASTHelpers;
 import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.ClassTree;
@@ -70,39 +69,24 @@ public final class JUnitValueSource extends BugChecker implements MethodTreeMatc
           .put("short", "shorts")
           .build();
   private static final Matcher<AnnotationTree> METHOD_SOURCE =
-      Matchers.isType("org.junit.jupiter.params.provider.MethodSource");
+      isType("org.junit.jupiter.params.provider.MethodSource");
 
   @Override
   public Description matchMethod(MethodTree tree, VisitorState state) {
-    AnnotationTree methodSourceAnnotation =
-        ASTHelpers.getAnnotations(tree).stream()
-            .filter(annotation -> METHOD_SOURCE.matches(annotation, state))
-            .findFirst()
-            .orElse(null);
-    if (methodSourceAnnotation == null) {
-      return Description.NO_MATCH;
-    }
-
-    String parameterType = hasValidParameters(tree).orElse(null);
-    if (parameterType == null) {
-      return Description.NO_MATCH;
-    }
-
-    String valueSource = getValueSource(parameterType, methodSourceAnnotation, state).orElse(null);
-    if (valueSource == null) {
-      return Description.NO_MATCH;
-    }
-
-    return describeMatch(
-        tree,
-        SuggestedFix.builder()
-            .addImport("org.junit.jupiter.params.provider.ValueSource")
-            .replace(methodSourceAnnotation, valueSource)
-            .delete(getMethodSourceMethod(methodSourceAnnotation, state).orElse(null))
-            .build());
+    return ASTHelpers.getAnnotations(tree).stream()
+        .filter(annotation -> METHOD_SOURCE.matches(annotation, state))
+        .findFirst()
+        .flatMap(annotation -> tryValueSourceFix(tree, annotation, state))
+        .orElse(Description.NO_MATCH);
   }
 
-  private static Optional<String> hasValidParameters(MethodTree tree) {
+  private Optional<Description> tryValueSourceFix(
+      MethodTree tree, AnnotationTree annotation, VisitorState state) {
+    return getParameterType(tree)
+        .flatMap(type -> tryConstructValueSourceFix(tree, type, annotation, state));
+  }
+
+  private static Optional<String> getParameterType(MethodTree tree) {
     if (tree.getParameters().isEmpty() || tree.getParameters().size() > 1) {
       return Optional.empty();
     }
@@ -111,8 +95,11 @@ public final class JUnitValueSource extends BugChecker implements MethodTreeMatc
         .map(type -> VALID_PARAMETER_TYPES.get(type.tsym.getQualifiedName().toString()));
   }
 
-  private static Optional<String> getValueSource(
-      String parameterType, AnnotationTree methodSourceAnnotation, VisitorState state) {
+  private Optional<Description> tryConstructValueSourceFix(
+      MethodTree methodTree,
+      String parameterType,
+      AnnotationTree methodSourceAnnotation,
+      VisitorState state) {
     MethodTree methodSourceTree = getMethodSourceMethod(methodSourceAnnotation, state).orElse(null);
     if (methodSourceTree == null) {
       return Optional.empty();
@@ -130,13 +117,22 @@ public final class JUnitValueSource extends BugChecker implements MethodTreeMatc
 
     ImmutableList<String> arguments =
         invocationTree.getArguments().stream()
-            .filter(argument -> argument.getKind() == METHOD_INVOCATION)
+            .filter(MethodInvocationTree.class::isInstance)
             .map(MethodInvocationTree.class::cast)
             .map(invocation -> collectValues(invocation, state))
             .collect(toImmutableList());
 
     return Optional.of(
-        String.format("@ValueSource(%s = {%s})", parameterType, String.join(", ", arguments)));
+        describeMatch(
+            methodTree,
+            SuggestedFix.builder()
+                .addImport("org.junit.jupiter.params.provider.ValueSource")
+                .replace(
+                    methodSourceAnnotation,
+                    String.format(
+                        "@ValueSource(%s = {%s})", parameterType, String.join(", ", arguments)))
+                .delete(getMethodSourceMethod(methodSourceAnnotation, state).orElse(null))
+                .build()));
   }
 
   private static Optional<ReturnTree> getReturnTree(MethodTree methodSourceTree) {
